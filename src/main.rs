ad534a070b31e30b21967bf126ba163dbfb86a6e
Timothy Julian <timothyjulian2001@yahoo.co.id>
2025-06-18 23:42:16 +0700

    postgres test manual connect

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,17 @@
 fn main() {
     println!("Hello, world!");
 }
+
+#[cfg(test)]
+mod tests {
+    use sqlx::{Connection, Error, PgConnection};
+
+    #[tokio::test]
+    async fn test_manual_connection() -> Result<(), Error> {
+        let url = "postgres://timy:@localhost:5432/rust_test";
+        let connection = PgConnection::connect(url).await?;
+
+        connection.close().await?;
+        Ok(())
+    }
+}
