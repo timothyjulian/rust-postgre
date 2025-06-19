3f8bf8bd0c0299acdfaa11ce234456224cfc93c7
Timothy Julian <timothyjulian2001@yahoo.co.id>
2025-06-20 00:17:29 +0700

    db pool

--- src/main.rs
+++ src/main.rs
@@ -4,7 +4,19 @@ fn main() {
 
 #[cfg(test)]
 mod tests {
-    use sqlx::{Connection, Error, PgConnection};
+    use std::time::Duration;
+
+    use sqlx::{postgres::PgPoolOptions, Connection, Error, PgConnection, PgPool, Pool, Postgres};
+
+    async fn get_pool() -> Result<Pool<Postgres>, Error> {
+        let url = "postgres://timy:@localhost:5432/rust_test";
+        PgPoolOptions::new()
+            .max_connections(10)
+            .min_connections(5)
+            .acquire_timeout(Duration::from_secs(5))
+            .idle_timeout(Duration::from_secs(60))
+            .connect(url).await
+    }
 
     #[tokio::test]
     async fn test_manual_connection() -> Result<(), Error> {
@@ -12,6 +24,13 @@ mod tests {
         let connection = PgConnection::connect(url).await?;
 
         connection.close().await?;
+        Ok(())
+    }
+
+    #[tokio::test]
+    async fn test_pool_connection() -> Result<(), Error> {
+        let pool = get_pool().await?;
+        pool.close().await;
         Ok(())
     }
 }
